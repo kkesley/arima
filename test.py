a94715d2aecdc5e274905996f876189fb2e4501e
kkesley <kendrick.kesley.dev@gmail.com>
2018-09-19 22:33:03 +1000

    init

---
+++ test.py
@@ -0,0 +1,31 @@
+
+from pandas import read_csv
+from pandas import datetime
+from matplotlib import pyplot
+from statsmodels.tsa.arima_model import ARIMA
+from sklearn.metrics import mean_squared_error
+
+def parser(x):
+	return datetime.strptime(x + "", '%d/%m/%Y')
+ 
+series = read_csv('test.csv', parse_dates=[0, 1], index_col=[0,1], usecols=[0,1,3])
+X = series.values
+size = int(len(X) * 0.66)
+train, test = X[0:size], X[size:len(X)]
+history = [x for x in train]
+predictions = list()
+for t in range(len(test)):
+	model = ARIMA(history, order=(5,1,0))
+	model_fit = model.fit(disp=0)
+	output = model_fit.forecast()
+	yhat = output[0]
+	predictions.append(yhat)
+	obs = test[t]
+	history.append(obs)
+	print('predicted=%f, expected=%f' % (yhat, obs))
+error = mean_squared_error(test, predictions)
+print('Test MSE: %.3f' % error)
+# plot
+pyplot.plot(test)
+pyplot.plot(predictions, color='red')
+pyplot.show()
